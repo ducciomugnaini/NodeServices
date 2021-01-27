526d496ef05bada53a2a98fb6654989fc463ce1c
Duccio Mugnaini <duccio.mugnaini@gmail.com>
2021-01-27 01:05:57 +0100

    init repo

---
+++ TestAddNodeServices/Controllers/WeatherForecastController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAddNodeServices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [Obsolete]
+        [HttpGet("TestNodeService")]
+        public async Task<string> TestNodeService([FromServices] INodeServices nodeServices)
+        {
+            var num1 = 10;
+            var num2 = 20;
+            var result = await nodeServices.InvokeAsync<int>("./Node/AddModule.js", num1, num2);
+            var resultTest = $"Result of {num1} + {num2} is {result}";
+
+            return resultTest;
+        }
+
+
+    }
+}
